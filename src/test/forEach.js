d3d70b62a61fc3228250b0b101e9bd019f1f69a2
Mihael_Cukon <mihael.cukon@gmail.com>
2021-06-09 12:36:03 +0200

    dodani primjeri za forEach

--- src/test/forEach.js
+++ src/test/forEach.js
@@ -15,4 +15,36 @@ describe("forEach exercise", function () {
       "salata od jabuke",
     ]);
   });
+  it("forEach test 2", function () {
+    let brojevi = [1, 2, 3];
+    brojevi.forEach(myfunction);
+    function myfunction(value, index, arr) {
+      arr[index] = value + 1;
+    }
+    expect(brojevi).to.eql([2, 3, 4]);
+  });
+  it("forEach test 3", function () {
+    let brojevi = [2, 3, 4];
+    brojevi.forEach(myfunction);
+    function myfunction(value, index, arr) {
+      arr[index] = value - 1;
+    }
+    expect(brojevi).to.eql([1, 2, 3]);
+  });
+  it("forEach test 4", function () {
+    let brojevi = [2, 3, 4];
+    brojevi.forEach(myfunction);
+    function myfunction(value, index, arr) {
+      arr[index] = value * 2;
+    }
+    expect(brojevi).to.eql([4, 6, 8]);
+  });
+  it("forEach test 5", function () {
+    let brojevi = [4, 6, 8];
+    brojevi.forEach(myfunction);
+    function myfunction(value, index, arr) {
+      arr[index] = value / 2;
+    }
+    expect(brojevi).to.eql([2, 3, 4]);
+  });
 });
